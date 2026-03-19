31c4f5de219bdc430f7a7fdf243ea08e3fa9c46c
Kzaa Decheyy <08250272sce@rub.edu.bt>
2026-03-19 16:29:59 +0530

    .\DEMO\

---
+++ DEMO/stringEx.py
@@ -0,0 +1,12 @@
+print("hello, world!")
+print("Kezang Dechen")  
+
+multiplelines = """This is a string
+ that spans multiple lines."""
+
+a = 1
+b = 2
+c = a + b
+print(type(c))
+c = str(c)
+print(type(c))
