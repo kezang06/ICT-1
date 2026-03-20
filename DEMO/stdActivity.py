95e9b782327edea691b7d26b52d8aba85b623d58
Kzaa Decheyy <08250272sce@rub.edu.bt>
2026-03-20 15:47:10 +0530

    .\stdActivity.py

---
+++ DEMO/stdActivity.py
@@ -0,0 +1,16 @@
+Name = input ("Enter your name: ")
+print("Hello ", Name)
+
+num1 = float(input ("Enter the num 1:"))
+print(num1)
+num2 = float(input ("Enter the num 2:"))
+print(num2)
+
+addition_result = num1 + num2
+subtraction_result = num1 - num2
+multiplication_result = num1 * num2
+division_result = num1 / num2
+print("add:", addition_result)
+print("sub:", subtraction_result)
+print("multiply", multiplication_result)
+print("divide:", division_result)
