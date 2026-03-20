3ce08d7c51bdf6234654dc096de11dfd5ea6c60c
Kzaa Decheyy <08250272sce@rub.edu.bt>
2026-03-20 15:11:39 +0530

    .\trueOrfalseEx.py

---
+++ DEMO/areaOfRectangle.py
@@ -0,0 +1,4 @@
+L = float(input("Enter the length of the rectangle: "))
+W = float(input("Enter the width of the rectangle: "))
+Area = L * W
+print("The area of the rectangle is:" + str(Area))
---
+++ DEMO/arithmeticOp.py
@@ -0,0 +1,14 @@
+a = 10
+b = 5
+addition_result = a + b
+subtraction_result = a - b
+multiplication_result = a * b
+division_result = a / b
+exponentiation_result = a ** b
+modulus_result = a % b
+print (addition_result)
+print (subtraction_result)
+print (multiplication_result)
+print (division_result)
+print(exponentiation_result)
+print(modulus_result)
