22081b444305f0d8786ef5bddccc8beccce39249
Kzaa Decheyy <08250272sce@rub.edu.bt>
2026-03-25 09:42:45 +0530

    conditionalOp.py

--- DEMO/conditionalOP.py
+++ DEMO/conditionalOP.py
@@ -2,6 +2,7 @@ Marks1 = float(input("Enter the marks of first subject"))
 Marks2 =float(input("Enter the marks of second subject"))
 Marks3 = float(input("Enter the marks of third subject"))
 average = (Marks1 + Marks2 + Marks3) / 3
+print("The average of 3 subject are: %.2f" % (average)) 
 
 if (average >= 90 and Marks1 >= 50 and Marks2 >= 50 and Marks3 >= 50):
     print("Grade A")
@@ -15,5 +16,5 @@ elif (average >= 50 and Marks1 >= 50 and Marks2 >= 50 and Marks3 >= 50):
      print("Grade E")
 else:
      print("You need to work harder")
-     
+
     
